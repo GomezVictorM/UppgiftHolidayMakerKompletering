9cb3e0f08bd0bdbc0acf87c62e902c01ecc5430c
GomezVictorM <victor.gomcha@gmail.com>
2020-07-12 23:42:06 +0200

    Issues

--- src/DatabasKurs_VictorGomez/Query.java
+++ src/DatabasKurs_VictorGomez/Query.java
@@ -61,7 +61,7 @@ public class Query {
             this.statement.setString(6, roomID);
             this.statement.executeUpdate();
             System.out.println("Booking added to the database");
-            return getIDOfBookingFrom(userId, checkIn, checkOut, roomID);
+            return getIDOfBooking(userId, checkIn, checkOut, roomID);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
